ec1082cd86e18253450c24f87c2b9336edbe696a
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-09-20 16:32:35 +0100

    Add short about us section.

---
+++ components/HomeAboutUs.vue
@@ -0,0 +1,39 @@
+<template>
+	<section class="section section-decorated-1">
+		<div class="decor-1">
+			<NuxtImg src="bubbles-219x209.png" alt="" width="219" height="209" loading="lazy" />
+		</div>
+		<div class="decor-2">
+			<NuxtImg src="bubbles-224x306.png" alt="" width="224" height="306" loading="lazy" />
+		</div>
+		<div class="container">
+			<div class="row row-50 justify-content-center justify-content-lg-between flex-lg-row-reverse align-items-center">
+				<div class="col-md-10 col-lg-6 col-xl-5">
+					<h3 class="aos-outer">
+						<span class="aos" data-aos="slide-in-down">A Few Words About Us</span>
+					</h3>
+					<p class="aos-outer">
+						<span class="aos" data-aos="slide-in-down" data-aos-delay="500">SpaDog is a pet salon established in 1999 with the main goal of giving your pets the full furry body treatment so that they can look their very best every day.</span>
+					</p>
+					<p class="aos-outer">
+						<span class="aos" data-aos="slide-in-down" data-aos-delay="1000">Our salon offers a complete line of fine grooming services to bring out the best of appearance and personality in your pet. Our groomers work carefully, never rush, and do not overbook appointments.</span>
+					</p>
+					<div class="aos-outer button-outer">
+						<a class="button button-lg button-primary button-winona" data-aos-delay="1000" href="about-us.html">Learn more</a>
+					</div>
+				</div>
+				<div class="col-md-10 col-lg-6 aos-outer">
+					<NuxtImg class="img-responsive aos" data-aos="slide-in-left" src="about-501-526.jpg" alt="" loading="lazy" />
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
