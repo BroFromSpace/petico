789b82413f864b241b738ef9fc3c65735131912c
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-09-22 10:22:31 +0100

    Finish short "Services" section.

---
+++ components/HomeServices.vue
@@ -0,0 +1,97 @@
+<template>
+	<section class="section-lg text-center">
+		<div class="container">
+			<h3 class="aos-outer">
+				<span class="aos" data-aos="slide-in-up">Services</span>
+			</h3>
+			<p class="aos-outer">
+				<span class="aos text-width-1" data-aos="slide-in-down">We offer a variety of grooming services for dogs of all breeds and ages. We use only high-quality products that suit the needs of your pet. Discover our most popular services below.</span>
+			</p>
+			<div class="row row-30 offset-top-2">
+				<div class="col-sm-6 col-lg-3 aos-outer">
+					<article class="thumbnail-light aos" data-aos="slide-in-left">
+						<a class="thumbnail-light-media" href="single-service.html">
+							<NuxtImg
+								class="thumbnail-light-image"
+								src="service-thumbnail-1-270x200.jpg"
+								alt=""
+								width="270"
+								height="200"
+								loading="lazy"
+							/>
+						</a>
+						<h5 class="thumbnail-light-title">
+							<a href="single-service.html">Styling</a>
+						</h5>
+					</article>
+				</div>
+				<div class="col-sm-6 col-lg-3 aos-outer">
+					<article class="thumbnail-light aos" data-aos="slide-in-left" data-aos-delay="50">
+						<a class="thumbnail-light-media" href="single-service.html">
+							<NuxtImg
+								class="thumbnail-light-image"
+								src="service-thumbnail-2-270x200.jpg"
+								alt=""
+								width="270"
+								height="200"
+								loading="lazy"
+							/>
+						</a>
+						<h5 class="thumbnail-light-title">
+							<a href="single-service.html">Full Grooming</a>
+						</h5>
+					</article>
+				</div>
+				<div class="col-sm-6 col-lg-3 aos-outer">
+					<article class="thumbnail-light aos" data-aos="slide-in-left" data-aos-delay="100">
+						<a class="thumbnail-light-media" href="single-service.html">
+							<NuxtImg
+								class="thumbnail-light-image"
+								src="service-thumbnail-3-270x200.jpg"
+								alt=""
+								width="270"
+								height="200"
+								loading="lazy"
+							/>
+						</a>
+						<h5 class="thumbnail-light-title">
+							<a href="single-service.html">Bath & Tidy Up</a>
+						</h5>
+					</article>
+				</div>
+				<div class="col-sm-6 col-lg-3 aos-outer">
+					<article class="thumbnail-light aos" data-aos="slide-in-left" data-aos-delay="150">
+						<a class="thumbnail-light-media" href="single-service.html">
+							<NuxtImg
+								class="thumbnail-light-image"
+								src="service-thumbnail-4-270x200.jpg"
+								alt=""
+								width="270"
+								height="200"
+								loading="lazy"
+							/>
+						</a>
+						<h5 class="thumbnail-light-title">
+							<a href="single-service.html">Deluxe Bath</a>
+						</h5>
+					</article>
+				</div>
+			</div>
+		</div>
+		<div class="aos-outer button-outer offset-top-2">
+			<a class="button button-primary-outline button-winona offset-top-2 aos" data-aos="slide-in-up" href="services.html">
+				<WinonaContent>
+					View all services
+				</WinonaContent>
+			</a>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
