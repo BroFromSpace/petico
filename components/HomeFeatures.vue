c25d0860511901fc9d16e4603194356bba94b8cf
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-09-22 10:21:33 +0100

    Finish short "Features" section.

---
+++ components/HomeFeatures.vue
@@ -0,0 +1,77 @@
+<template>
+	<section class="section section-md bg-gray-light">
+		<div class="container">
+			<div class="row row-40 align-items-center">
+				<div class="col-xl-8">
+					<div class="row row-xl-60">
+						<div class="col-sm-6 aos-outer">
+							<article class="box-minimal">
+								<div class="box-minimal-icon linearicons-star aos" data-aos="fade-in" />
+								<div class="box-minimal-main aos-outer">
+									<h4 class="box-minimal-title aos" data-aos="slide-in-down">
+										V.I.P. Service
+									</h4>
+									<p class="aos" data-aos="fade-in-up-small">
+										Our team provides grooming services of the highest quality to not only make your dog look amazing but also make them feel special.
+									</p>
+								</div>
+							</article>
+						</div>
+						<div class="col-sm-6 aos-outer">
+							<article class="box-minimal">
+								<div class="box-minimal-icon linearicons-heart aos" data-aos="fade-in" data-aos-delay="100" />
+								<div class="box-minimal-main aos-outer">
+									<h4 class="box-minimal-title aos" data-aos="slide-in-down" data-aos-delay="100">
+										We Love Every Pet
+									</h4>
+									<p class="aos" data-aos="fade-in-up-small" data-aos-delay="100">
+										Every pet we work with receives the same level of love and care when being treated by our specialists. That’s why pet owners trust us.
+									</p>
+								</div>
+							</article>
+						</div>
+						<div class="col-sm-6 aos-outer">
+							<article class="box-minimal">
+								<div class="box-minimal-icon linearicons-scissors aos" data-aos="fade-in" data-aos-delay="100" />
+								<div class="box-minimal-main aos-outer">
+									<h4 class="box-minimal-title aos" data-aos="slide-in-down" data-aos-delay="200">
+										Top Groomers
+									</h4>
+									<p class="aos" data-aos="fade-in-up-small" data-aos-delay="200">
+										Our team consists of more than 20 qualified and experienced groomers, aimed at providing stellar results for our customers.
+									</p>
+								</div>
+							</article>
+						</div>
+						<div class="col-sm-6 aos-outer">
+							<article class="box-minimal">
+								<div class="box-minimal-icon linearicons-trophy2 aos" data-aos="fade-in" data-aos-delay="200" />
+								<div class="box-minimal-main aos-outer">
+									<h4 class="box-minimal-title aos" data-aos="slide-in-down" data-aos-delay="200">
+										Best Level of Service
+									</h4>
+									<p class="aos" data-aos="fade-in-up-small" data-aos-delay="200">
+										At SpaDog, we provide top-notch customer service and support to everyone who enters our salon, from pets to their owners.
+									</p>
+								</div>
+							</article>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-4">
+					<div class="offset-negative-70">
+						<NuxtImg src="home-2-386x503.jpg" alt="" width="386" height="503" loading="lazy" />
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
