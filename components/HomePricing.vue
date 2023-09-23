f24aa4ea89003f5dfea16ac1707e63fa011ab6d4
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-09-24 00:03:34 +0100

    Add short pricing section.

---
+++ components/HomePricing.vue
@@ -0,0 +1,58 @@
+<template>
+	<section class="section section-lg bg-gray-light section-decorated-7">
+		<div class="decor-1">
+			<NuxtImg src="bubbles-171x230.png" alt="" width="171" height="230" loading="lazy" />
+		</div>
+		<div class="container">
+			<h3 class="aos-outer text-center">
+				<span class="aos" data-aos="slide-in-down">Pricing</span>
+			</h3>
+			<div class="row row-30">
+				<div class="col-sm-6 col-lg-4 aos-outer">
+					<article class="pricing-minimal aos" data-aos="slide-in-right">
+						<h5 class="pricing-minimal-title">
+							<a href="single-service.html">Basic Package</a>
+						</h5>
+						<p class="pricing-minimal-price">
+							<span class="pricing-minimal-price-currency">$</span>49.00
+						</p>
+						<div class="pricing-minimal-divider" />
+						<p>Our basic package is an important part of dog grooming as it offers affordable bathing and brushing for dogs of any breed.</p><a class="button button-primary button-winona" href="single-service.html">Order now</a>
+					</article>
+				</div>
+				<div class="col-sm-6 col-lg-4 aos-outer">
+					<article class="pricing-minimal aos" data-aos="slide-in-right" data-aos-delay="50">
+						<h5 class="pricing-minimal-title">
+							<a href="single-service.html">Advanced Package</a>
+						</h5>
+						<p class="pricing-minimal-price">
+							<span class="pricing-minimal-price-currency">$</span>67.00
+						</p>
+						<div class="pricing-minimal-divider" />
+						<p>Our upgraded package is designed for dogs who need a little extra TLC by keeping skin moisturized, using a specialized shampoo & conditioner.</p><a class="button button-primary button-winona" href="single-service.html">Order now</a>
+					</article>
+				</div>
+				<div class="col-sm-6 col-lg-4 aos-outer">
+					<article class="pricing-minimal aos" data-aos="slide-in-right" data-aos-delay="100">
+						<h5 class="pricing-minimal-title">
+							<a href="single-service.html">Pro Package</a>
+						</h5>
+						<p class="pricing-minimal-price">
+							<span class="pricing-minimal-price-currency">$</span>86.00
+						</p>
+						<div class="pricing-minimal-divider" />
+						<p>This ultimate spa package is featured for the spoiled pup who needs the works and wants to enjoy a day of relaxation and beauty.</p><a class="button button-primary button-winona" href="single-service.html">Order now</a>
+					</article>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
