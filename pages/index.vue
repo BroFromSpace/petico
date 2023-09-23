a8afe4a3db0d190ef70e91eb766c2f268c43092a
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-09-24 00:04:24 +0100

    Add Pricing and CTA sections. Add short comments per each section.

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,24 @@
 <template>
 	<div>
+		<!-- Preview Slider -->
 		<HomeSwiper />
 
+		<!-- Short About Us Section -->
 		<HomeAboutUs />
 
+		<!-- Short Features Section -->
 		<HomeFeatures />
 
+		<!-- Short Services Section -->
 		<HomeServices />
 
+		<!-- Thin CTA -->
+		<HomeCTA />
+
+		<!-- Short Pricing Section -->
+		<HomePricing />
+
+		<!-- Short Testimonials Section -->
 		<HomeTestimonials />
 	</div>
 </template>
