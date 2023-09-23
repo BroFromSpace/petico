e11782d04bcfdb4c6d736ac44c84ba3cba347b56
Kyrylo Korablov <kyrylo.korablov@gmail.com>
2023-09-24 00:03:47 +0100

    Add thin CTA section.

---
+++ components/HomeCTA.vue
@@ -0,0 +1,29 @@
+<template>
+	<section class="section section-xs bg-primary-darker text-center section-decorated-2">
+		<div class="decor-1">
+			<NuxtImg src="bubbles-225x463.png" alt="" width="225" height="463" loading="lazy" />
+		</div>
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-sm-10 col-md-12">
+					<div class="box-cta-thin">
+						<h4 class="aos-outer">
+							<span class="aos" data-aos="slide-in-right">Qualified Personal Care For Your Pets</span>
+						</h4>
+						<div class="aos-outer button-outer">
+							<a class="button button-primary button-winona aos" data-aos="slide-in-left" href="pricing.html">View all pricing</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
